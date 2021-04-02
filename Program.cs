17b94d669f258ea68854cc5812ffe87a6cba06c9
ADVallespir <advallespir@gmail.com>
2021-04-02 14:24:51 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Guia_1_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa un texto para convertir en mayusculas");
+            string texto = Console.ReadLine();
+            string textoToUpper = texto.ToUpper(new System.Globalization.CultureInfo("es-ES", false));
+            Console.WriteLine(textoToUpper);
+            Console.ReadLine();
+
+        }
+    }
+}
